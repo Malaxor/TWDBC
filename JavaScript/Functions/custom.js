268b5f374b58f50a60bf5e639981baa64eab824b
Malaxor <nogobaduk@gmail.com>
2018-02-21 22:30:11 -0700

    completed the functions section

---
+++ JavaScript/Functions/custom.js
@@ -0,0 +1,35 @@
+
+function isEven(num) {
+
+	return num % 2 === 0;
+}
+
+//=========================================
+
+function factorial(num1) {
+
+	var result = 1;
+
+	for(var i = 2; i <= num1; i++) {
+
+		result*= i;
+	}
+
+	return result;
+}
+
+//=========================================
+
+function kebabToSnake(str) {
+
+	var newStr = str.replace(/-/g, "_");
+	return newStr;
+}
+
+
+
+
+
+
+
+
